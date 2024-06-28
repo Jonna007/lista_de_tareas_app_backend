06babc94cb6c87d649af06e6123d4d141c4fa31a
Jonna007 <146044709+Jonna007@users.noreply.github.com>
2024-06-27 19:13:40 -0500

    ok.
    mm

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   // Escuchar en el puerto 3000
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
